3bd7963b490ac32628ba8fe2768303206e5f03f2
ivancastanop <107499323+ivancastanop@users.noreply.github.com>
2024-11-25 09:18:03 -0500

    The player ship was created

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 
 def main():
@@ -10,11 +11,14 @@ def main():
     print("Starting asteroids!")
     # print(f"Screen width: {SCREEN_WIDTH}")
     # print(f"Screen height: {SCREEN_HEIGHT}")
+    player_avatar = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0, 0, 0))
+        player_avatar.draw(screen)
         pygame.display.flip()
         time_lapse = clock.tick(60)
         dt = time_lapse / 1000
